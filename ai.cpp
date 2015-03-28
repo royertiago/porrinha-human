72506dff75d056ad1627049959b7bdc5581243de
Tiago Royer <royertiago@gmail.com>
2015-03-28 00:18:40 -0300

    Add configuration files.

---
+++ ai.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "human_player.h"
+#include "ai.h"
+
+namespace human_player {
+
+    Player * generate( int /*players*/, int chopsticks ) {
+        std::string name;
+        std::cout << "Type the player's name:\n";
+        std::getline( std::cin, name );
+        return new HumanPlayer( name, chopsticks );
+    }
+
+} // namespace human_player
---
+++ ai.h
@@ -0,0 +1,3 @@
+namespace human_player {
+    Player * generate( int players, int chopsticks );
+} // namespace human_player
